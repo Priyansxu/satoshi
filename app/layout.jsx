f5578d7aca836e4b22a8f61834f475ee0ed5ac91
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-21 17:57:17 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.jpg" />
       </head>
       <body>{children}</body>
     </html>
