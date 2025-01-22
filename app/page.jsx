8cf1c0e875a7f5dacb7f0a9092c92251f84463cd
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-22 22:28:53 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import DownArrow from "@/components/DownArrow";
 
 export default function Page() {
   return (
-  <div className="min-h-screen bg-white/10 text-[#61577C] backdrop-blur-2xl">
+  <div className="min-h-screen bg-white/10 text-[#61577C] backdrop-blur-md">
    <Header />
    <DownArrow width={200} height={200} fill="#AB9FF1" />
    <Converter />
