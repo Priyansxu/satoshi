6332178a30107e073933651be0a437d76c78a8c0
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-22 16:04:47 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,11 @@ import Header from "@/components/Header";
 
 export default function Page() {
   return (
+ <>
   <div className="min-h-screen bg-[#F4F2FF] text-[#61577C]">
    <Header >
    <Converter />
   </div>
+ </>
 );
 }
