54b1ab8b360c4a19c7f31d2af709c7581eb79c7d
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-22 23:28:54 +0530

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,7 +2,7 @@ import { Bitcoin } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 backdrop-blur-md bg-white/30 outline-double outline-b outline-2 outline-black/30">
+    <header className="sticky top-0 z-50 backdrop-blur-md bg-white/30 outline outline-b outline-6 outline-[#ab9ff1]/30">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         <div className="flex items-center space-x-2">
           <Bitcoin className="w-8 h-8 text-[#AB9FF1]" />
