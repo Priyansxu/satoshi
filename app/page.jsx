57df12448f5e6b0b02fc089380ed65d137fcc958
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-22 16:03:03 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
 import Converter from "@/components/Converter";
+import Header from "@/components/Header";
+
 
 export default function Page() {
   return (
-  <div className="flex justify-center items-center min-h-screen bg-[#F4F2FF] font-sans text-[#61577C] p-4">
+  <div className="min-h-screen bg-[#F4F2FF] text-[#61577C]">
+   <Header >
    <Converter />
   </div>
 );
