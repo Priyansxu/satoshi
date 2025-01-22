279d250a2fde9dc025efa59aa7e662fce4dcc6e4
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-22 15:51:38 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Converter from "@/components/Converter";
 
 export default function Page() {
   return (
-  <div className="flex justify-center items-center min-h-screen bg-gray-700 font-sans text-[#61577C] p-4">
+  <div className="flex justify-center items-center min-h-screen bg-[#F4F2FF] font-sans text-[#61577C] p-4">
    <Converter />
   </div>
 );
