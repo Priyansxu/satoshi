1f6850d30eac9fe00d5b874a4623d782a94a1ea5
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-22 16:39:04 +0530

    Update DownArrow.jsx

--- app/components/DownArrow.jsx
+++ app/components/DownArrow.jsx
@@ -3,6 +3,7 @@ export default function DownArrow(props) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 100 125"
+      transform="rotate(90, 50, 50)"
       {...props}
     >
       <title>Down Arrow</title>
