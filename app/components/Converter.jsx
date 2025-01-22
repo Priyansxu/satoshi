d55db41a7017767154dcc7a2a0bacc8c3316baa9
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-22 14:25:08 +0530

    Update Converter.jsx

--- app/components/Converter.jsx
+++ app/components/Converter.jsx
@@ -92,7 +92,7 @@ export default function Converter() {
 
           {["satoshi", "bitcoin", "currencyValue"].map((field) => (
             <div key={field} className="mb-6 relative">
-              <label htmlFor={field} className="block text-sm font-bold mb-2 text-opacity-80">
+              <label htmlFor={field} className="block text-sm font-bold mb-2 text-opacity-50">
                 {field === "currencyValue" ? currency.code.toUpperCase() : field.charAt(0).toUpperCase() + field.slice(1)}
               </label>
               <input
