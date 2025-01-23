432a37d670b0041ddb53f51c2bcdcb2e4e694018
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-23 18:47:09 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ import Donation from "@/components/Donation";
 
 export default function Page() {
   return (
+ <h1 className="text-md font-bold tracking-tight text-gradient-to-br from-[#AB9FF1] to-white">Convert Sats between Bitcoin and Currencies</h1><span className="text-white">between Bitcoin and Currencies</span>
   <div className="min-h-screen bg-white/10 text-[#61577C]">
    <Header />
     <DownArrow width={200} height={200} fill="#AB9FF1" />
