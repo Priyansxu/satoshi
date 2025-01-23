7ad17b0c407b5bae490318b6035c9be71c33f5b8
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-23 23:10:10 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,8 @@ export default function Page() {
   return (
     <div className="min-h-screen bg-white/10 text-[#61577C]">
       <Header />
-        <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-[#AB9FF1] to-[#F4F2FF] text-transparent bg-clip-text mt-6 mx-6 text-center">Convert Sats</h1>
-        <p className="text-xl font-semibold text-gray-800/70 font-mono mx-6 text-center">Between Bitcoin and Currencies</p>
+        <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-[#AB9FF1] to-[#F4F2FF] text-transparent bg-clip-text mt-10 mx-6 text-center">Convert Sats</h1>
+        <p className="text-xl font-semibold text-gray-800/60 font-mono mx-6 text-center">Between Bitcoin and Currencies</p>
       <DownArrow width={200} height={200} fill="#AB9FF1" />
       <Converter />
       <Donation />
