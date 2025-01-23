f3e763ad24a3a3a3578a4469c494253f3742d66d
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-23 20:54:24 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,13 +7,13 @@ import Donation from "@/components/Donation";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-white/10 text-[#61577C] flex flex-col items-center justify-center">
+    <div className="min-h-screen bg-white/10 text-[#61577C]">
       <Header />
-      <div className="mt-16 mb-8 p-6 bg-white/20 backdrop-blur-md rounded-lg shadow-lg text-center max-w-4xl">
+      <div className="mt-6 mb-8 p-4 bg-white/20 backdrop-blur-md rounded-lg shadow-lg text-center max-w-4xl">
         <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-[#AB9FF1] to-[#F4F2FF] text-transparent bg-clip-text mb-4">
           Convert Sats
         </h1>
-        <p className="text-xl font-semibold text-gray-200">
+        <p className="text-xl font-semibold text-gray-800">
           Between Bitcoin and Currencies
         </p>
       </div>
