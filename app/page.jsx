885bbfde3bd6a2f9dd68eaf696a83134a6ce4e6d
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-23 18:52:43 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,14 +7,12 @@ import Donation from "@/components/Donation";
 
 export default function Page() {
   return (
-  <>
- <h1 className="text-md font-bold tracking-tight text-gradient-to-br from-[#AB9FF1] to-white">Convert Sats<span className="text-white">between Bitcoin and Currencies</span></h1>
   <div className="min-h-screen bg-white/10 text-[#61577C]">
    <Header />
+  <h1 className="text-lg font-bold tracking-tight text-gradient-to-br from-[#AB9FF1] to-white">Convert Sats <br><span className="text-white">between Bitcoin and Currencies</span></h1>
     <DownArrow width={200} height={200} fill="#AB9FF1" />
     <Converter />
     <Donation />
    </div>
-  </>
  );
 }
