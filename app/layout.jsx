f30d396259de8d87b94d8d39dee52efaae166e89
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-23 20:30:45 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import './globals.css';
 export const metadata = {
   title: 'Satoshi Converter',
   description: 'Satoshi Converter into Bitcoin and other currency.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({ children }) {
