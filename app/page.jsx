c27d90e335fad050c1e07b3aeb723cf6f2ee2b60
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-24 15:21:30 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Header from "@/components/Header";
+import Header from "@/components/Header";
 import DownArrow from "@/components/DownArrow";
 import Converter from "@/components/Converter";
 import Donation from "@/components/Donation";
@@ -9,8 +10,9 @@ export default function Page() {
   return (
     <div className="min-h-screen bg-white/10 text-[#61577C]">
       <Header />
-      <DownArrow width={200} height={200} fill="#AB9FF1" />
-      <Converter />
+       <Intro />
+        <DownArrow width={200} height={200} fill="#AB9FF1" />
+       <Converter />
       <Donation />
     </div>
   );
