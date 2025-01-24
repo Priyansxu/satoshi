726ef7593ad99c497b10e1300eb86e0bb4a5641e
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-24 15:13:44 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,16 +7,8 @@ import Donation from "@/components/Donation";
 
 export default function Page() {
   return (
+    <div className="min-h-screen bg-white/10 text-[#61577C]">
       <Header />
-    <div className="min-h-screen bg-white/10 text-[#61577C] flex flex-col items-center justify-center">
-      <div className="bg-white/40 rounded-lg p-6 mt-10 w-full max-w-md text-center">
-        <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-[#AB9FF1] to-[#F4F2FF] text-transparent bg-clip-text">
-          Convert Sats
-        </h1>
-        <p className="mt-4 text-xl font-semibold text-gray-800/60 font-mono">
-          Between Bitcoin and Currencies
-        </p>
-      </div>
       <DownArrow width={200} height={200} fill="#AB9FF1" />
       <Converter />
       <Donation />
