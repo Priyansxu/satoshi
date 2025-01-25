5b0bc48a75b1d56083625975b2cdeadae0997898
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-25 14:04:12 +0530

    Update Intro.jsx

--- app/components/Intro.jsx
+++ app/components/Intro.jsx
@@ -1,10 +1,10 @@
 export default function Intro() {
   return (
      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-[#AB9FF1] to-[#F4F2FF] text-transparent bg-clip-text mx-8 mt-6">
+        <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-[#AB9FF1] to-[#F4F2FF] text-transparent bg-clip-text mx-8 mt-10">
           Convert Sats
         </h1>
-        <p className="mx-8 mt-2 text-md font-semibold text-gray-800/50 font-mono">
+        <p className="mx-8 text-md font-semibold text-gray-800/50 font-mono">
           Between Bitcoin and Currencies
         </p>
     </div>
