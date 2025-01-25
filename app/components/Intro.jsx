7153392a962d9e65f9cf92f7d78bc87f930d7257
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-25 14:07:00 +0530

    Update Intro.jsx

--- app/components/Intro.jsx
+++ app/components/Intro.jsx
@@ -1,7 +1,7 @@
 export default function Intro() {
   return (
      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-[#AB9FF1] to-[#F4F2FF] text-transparent bg-clip-text mx-8 mt-10 border-[#AB9FF1]">
+        <h1 className="text-5xl font-extrabold tracking-tight bg-gradient-to-br from-[#AB9FF1] to-[#F4F2FF] text-transparent bg-clip-text mx-8 mt-10 border-[#AB9FF1] border-2">
           Convert Sats
         </h1>
         <p className="mx-8 text-md font-semibold text-gray-800/50 font-mono">
