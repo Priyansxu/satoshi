1986cb0b02560fe130e4e0b94c9a99382774408c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-28 22:47:05 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Head from "next/head";
 import Header from "@/components/Header";
 import Intro from "@/components/Intro";
 import DownArrow from "@/components/DownArrow";
@@ -8,12 +9,20 @@ import Donation from "@/components/Donation";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-white/10 text-[#61577C]">
-      <Header />
-       <Intro />
+    <>
+      <Head>
+        <title>Satoshi Calculator</title>
+        <meta name="description" content="Bitcoin Unit Converter: Convert between Satoshi, Bitcoin, and major currencies instantly" />
+        <meta name="theme-color" content="#AB9FF1" />
+        <link rel="icon" href="/logo.jpg" />
+      </Head>
+      <div className="min-h-screen bg-white/10 text-[#61577C]">
+        <Header />
+        <Intro />
         <DownArrow width={200} height={200} fill="#AB9FF1" />
-       <Converter />
-      <Donation />
-    </div>
+        <Converter />
+        <Donation />
+      </div>
+    </>
   );
 }
