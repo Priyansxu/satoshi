855c0d061b5fcc14b49160cfb463995cd247afe9
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-01-29 15:49:39 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,9 @@ export const metadata = {
   title: 'Satoshi Converter',
   description: 'Bitcoin Unit Converter: Accurately convert between Satoshi, Bitcoin, and major currencies with precise and real-time rates.',
   themeColor: '#AB9FF1',
-  icons: '/logo.jpg',
+  icons: {
+  icon: '/favicon-32x32.png',
+ },
 };
 
 export default function RootLayout({ children }) {
